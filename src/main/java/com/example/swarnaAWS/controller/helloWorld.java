1e2bfb284127487e8ad652a520e505a36ea8311c
swarnapusuluri <swarnapusuluri@gmail.com>
2025-05-09 17:18:26 -0500

    welcome commit

--- src/main/java/com/example/swarnaAWS/controller/helloWorld.java
+++ src/main/java/com/example/swarnaAWS/controller/helloWorld.java
@@ -8,7 +8,7 @@ public class helloWorld {
 
     @GetMapping("/getName")
     public String printName(){
-        return "Hello Swarna!!! Welcomeeeee";
+        return "Hello Swarna!!! Welcomeeeeee";
 
     }
 }
