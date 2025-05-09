f6cb8d82075c21c64fbf33d204c5a2259e7aa347
swarnapusuluri <swarnapusuluri@gmail.com>
2025-05-09 17:10:00 -0500

    first commit

---
+++ src/main/java/com/example/swarnaAWS/SwarnaAwsApplication.java
@@ -0,0 +1,13 @@
+package com.example.swarnaAWS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwarnaAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwarnaAwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/swarnaAWS/SwarnaAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.swarnaAWS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwarnaAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
