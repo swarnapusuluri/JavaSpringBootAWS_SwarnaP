92463206a1e2fdc9e9ba741c914b7f32eed0efab
swarnapusuluri <swarnapusuluri@gmail.com>
2025-05-10 11:43:59 -0500

    update controller

--- src/main/java/com/example/swarnaAWS/controller/helloWorld.java
+++ src/main/java/com/example/swarnaAWS/controller/helloWorld.java
@@ -8,7 +8,7 @@ public class helloWorld {
 
     @GetMapping("/getName")
     public String printName(){
-        return "Hello Swarna!!! Welcomeeeeees";
+        return "Hello Swarna!!! Welcomeeeeee Swarna";
 
     }
 }
